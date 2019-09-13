7b4dbd370bb6ed66c94982b43c6f1b13dd470869
ipagar <ipagar@icloud.com>
2019-09-14 00:13:15 +0300

    Updates

---
+++ precache-manifest.d09a36b98cfa309d0a6acc3af12ce9e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d4960d54d5d58babd81242a9242b338",
+    "url": "/talktome/static/media/send.5d4960d5.svg"
+  },
+  {
+    "revision": "d61f592fbc62168fa521",
+    "url": "/talktome/static/js/runtime~main.d2975394.js"
+  },
+  {
+    "revision": "86677519942acb43682a",
+    "url": "/talktome/static/js/main.fa775caa.chunk.js"
+  },
+  {
+    "revision": "62fef31fe729d6ea072d",
+    "url": "/talktome/static/js/2.8de0840e.chunk.js"
+  },
+  {
+    "revision": "86677519942acb43682a",
+    "url": "/talktome/static/css/main.cfe410f7.chunk.css"
+  },
+  {
+    "revision": "62fef31fe729d6ea072d",
+    "url": "/talktome/static/css/2.bfec9101.chunk.css"
+  },
+  {
+    "revision": "b4f96859ae08da27823fa0d50d3f3dda",
+    "url": "/talktome/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec77683ad1899a339085ac171182d4d2.js"
+  "/talktome/precache-manifest.d09a36b98cfa309d0a6acc3af12ce9e6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/talktome/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
